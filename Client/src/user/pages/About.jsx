a751b9927f1d47ae323916ae7c98a71d4e008f59
Govind Manoj <govindmanoj333@gmail.com>
2025-02-08 17:50:56 +0530

    8-2-24

--- Client/src/user/pages/About.jsx
+++ Client/src/user/pages/About.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is a page about us.</p>
+    </div>
+  );
 };
 
 export default About;
